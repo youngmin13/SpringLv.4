1261a77fcd04a7be06264976aa3245ec2508db05
youngmin13 <wjsdudals56@naver.com>
2023-07-11 16:48:32 +0900

    init commit

---
+++ src/main/java/com/example/springlv_4/Application.java
@@ -0,0 +1,16 @@
+package com.example.springlv_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
